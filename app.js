75ecfa4afba7b39c5c8e4f3ab1157a6997f47b6d
atilaahmettaner <67838093+atilaahmettaner@users.noreply.github.com>
2023-05-03 21:18:04 +0300

    update port for heroku

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import fetch from "node-fetch";
 
 dotenv.config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 import {Configuration, OpenAIApi} from "openai";
 
